9440e0b1c9fde7885685320b7707f6e0f95407ea
Kai <kai.chen@unifr.ch>
2018-04-14 16:15:54 +0200

    update my solutions

--- booking-prediction.py
+++ booking-prediction.py
@@ -72,177 +72,70 @@ TARGET_ACTION_FILE_PATH = 'data/case_study_actions_target.csv'     # user action
 # -------------------
 # Step 1: read and explore the data
 #
+
+# ---
+# Define file paths
+TRAIN_BOOKING_FILE_PATH = 'data/case_study_bookings_train.csv'    # training sessions for bookings
+TARGET_BOOKING_FILE_PATH = 'data/case_study_bookings_target.csv'  # target sessions to predict bookings
+
+TRAIN_ACTION_FILE_PATH = 'data/case_study_actions_train.csv'       # training set of user actions
+TARGET_ACTION_FILE_PATH = 'data/case_study_actions_target.csv'     # user actions in the target sessions
+
+
 train_booking_df = pd.read_csv(TRAIN_BOOKING_FILE_PATH, sep='\t')
 train_booking_df['ymd'] = pd.to_datetime(train_booking_df['ymd'].astype('str'))
-
-print('train booking')
-print(train_booking_df.columns)
-print(train_booking_df.head(5))
 
 target_booking_df = pd.read_csv(TARGET_BOOKING_FILE_PATH, sep='\t')
 target_booking_df['ymd'] = pd.to_datetime(target_booking_df['ymd'].astype('str'))
 
-print('target booking')
-print(target_booking_df.columns)
-print(target_booking_df.head(10))
-
 train_user_id_list = train_booking_df['user_id'].unique()
 train_session_id_list = train_booking_df['session_id'].unique()
-
-print('\n---------------')
-print('number of users (train booking data): {}'.format(len(train_user_id_list)))
-print('number of sessions (tarin booking data): {}'.format(len(train_session_id_list)))
-print('dataframe size (train booking data)')
-print(train_booking_df.shape)
-print('---------------\n')
 
 target_user_id_list = target_booking_df['user_id'].unique()
 target_session_id_list = target_booking_df['session_id'].unique()
 
-print('\n---------------')
-print('number of users (target booking data): {}'.format(len(target_user_id_list)))
-print('number of sessions (target booking data): {}'.format(len(target_session_id_list)))
-print('dataframe size (target booking data)')
-print(target_booking_df.shape)
-print('---------------\n')
-
-
-# print('---------------')
-# print(train_booking_df[train_booking_df['user_id'] == 388309106223940])
-# print('---------------')
-# print(train_booking_df[train_booking_df['user_id'] == 452426828488840])
-
-
 train_action_df = pd.read_csv(TRAIN_ACTION_FILE_PATH, sep='\t')
 train_action_df['ymd'] = pd.to_datetime(train_action_df['ymd'].astype('str'))
-
-print('train action')
-print(train_action_df.columns)
-print(train_action_df.head(5))
-
-train_user_id_action_list = train_action_df['user_id'].unique()
-train_session_id_action_list = train_action_df['session_id'].unique()
-
-print('---------------')
-print('number of users (train action data): {}'.format(len(train_user_id_action_list)))
-print('number of sessions (train action data): {}'.format(len(train_session_id_action_list)))
-print('dataframe size (train booking data)')
-print(train_action_df.shape)
-print('---------------')
-
 
 target_action_df = pd.read_csv(TARGET_ACTION_FILE_PATH, sep='\t')
 target_action_df['ymd'] = pd.to_datetime(target_action_df['ymd'].astype('str'))
 
-print('target action')
-print(target_action_df.columns)
-print(target_action_df.head(5))
 
-target_user_id_action_list = target_action_df['user_id'].unique()
-target_session_id_action_list = target_action_df['session_id'].unique()
+# replace the NAN values by a specific value
+NA_ACTION_ID = -10
+NA_REFERENCE_ID = -10
+NA_STEP = 0
 
-print('---------------')
-print('number of users (target action data): {}'.format(len(target_user_id_action_list)))
-print('number of sessions (target action data): {}'.format(len(target_session_id_action_list)))
-print('dataframe size (target booking data)')
-print(target_action_df.shape)
-print('---------------')
+train_user_df = pd.merge(train_booking_df, train_action_df, on=['ymd', 'user_id', 'session_id'], how='left')
 
+train_user_df['action_id'].fillna(NA_ACTION_ID, inplace=True)
+train_user_df['reference'].fillna(NA_REFERENCE_ID, inplace=True)
+train_user_df['step'].fillna(NA_STEP, inplace=True)
 
-# check how many users who do not have action information
-# user_id_no_action_list = []
-# print('---------------')
-# print('user id (train)')
-# for user_id in train_user_id_list:
-#     if user_id not in train_user_id_action_list:
-#         user_id_no_action_list.append(user_id)
-# print('{} users (train) with no action information'.format(len(user_id_no_action_list)))
-#
-# check how many sessions which do not have action information
-# session_id_no_action_list = []
-# print('---------------')
-# print('session id')
-# for session_id in train_session_id_list:
-#     if session_id not in train_session_id_action_list:
-#         session_id_no_action_list.append(session_id)
-# print('{} sessions (train) with no action information'.format(len(session_id_no_action_list)))
+train_user_df['action_id'] = train_user_df['action_id'].astype('int')
+train_user_df['reference'] = train_user_df['reference'].astype('int')
+train_user_df['step'] = train_user_df['step'].astype('int')
 
-
-train_user_df =  pd.merge(train_booking_df, train_action_df, on=['ymd', 'user_id', 'session_id'])
+print('-------------------')
+print(train_user_df.columns)
 print('train user df shape')
 print(train_user_df.shape)
-# train_user_df =  pd.merge(train_booking_df, train_action_df, on=['session_id'])
-# print(train_user_df.shape)
-print(train_user_df.columns)
-print(train_user_df.head(5))
-print('ymd (train)')
-print(train_user_df['ymd'].unique())
 
-target_user_df =  pd.merge(target_booking_df, target_action_df, on=['ymd', 'user_id', 'session_id'])
+target_user_df = pd.merge(target_booking_df, target_action_df, on=['ymd', 'user_id', 'session_id'], how='left')
+
+target_user_df['action_id'].fillna(NA_ACTION_ID, inplace=True)
+target_user_df['reference'].fillna(NA_REFERENCE_ID, inplace=True)
+target_user_df['step'].fillna(NA_STEP, inplace=True)
+
+target_user_df['action_id'] = target_user_df['action_id'].astype('int')
+target_user_df['reference'] = target_user_df['reference'].astype('int')
+target_user_df['step'] = target_user_df['step'].astype('int')
+
+
+print('-------------------')
+print(target_user_df.columns)
 print('target user df shape')
 print(target_user_df.shape)
-# train_user_df =  pd.merge(train_booking_df, train_action_df, on=['session_id'])
-# print(train_user_df.shape)
-print(target_user_df.columns)
-print(target_user_df.head(5))
-print('ymd (target)')
-print(target_user_df['ymd'].unique())
-
-train_user_id_list = train_user_df['user_id'].unique()
-target_user_id_list = target_user_df['user_id'].unique()
-
-print('\ndifference between train user id and target user id')
-print(set(train_user_id_list) - set(target_user_id_list))
-# this shows that we could not use user id as a feature
-
-
-# print('---------------')
-# print(train_user_df[train_user_df['user_id']==388309106223940])
-
-# Reference
-# https://www.datascience.com/blog/introduction-to-correlation-learn-data-science-tutorials
-print('correlation')
-corr_score = train_user_df['referer_code'].corr(train_user_df['has_booking'])
-print('corr score of referer_code and has_booking {}'.format(corr_score))
-
-corr_score = train_user_df['is_app'].corr(train_user_df['has_booking'])
-print('corr score of is_app and has_booking {}'.format(corr_score))
-
-corr_score = train_user_df['agent_id'].corr(train_user_df['has_booking'])
-print('corr score of agent_id and has_booking {}'.format(corr_score))
-
-corr_score = train_user_df['traffic_type'].corr(train_user_df['has_booking'])
-print('corr score of traffic_type and has_booking {}'.format(corr_score))
-
-corr_score = train_user_df['action_id'].corr(train_user_df['has_booking'])
-print('corr score of action_id and has_booking {}'.format(corr_score))
-
-
-
-def get_nb_bookings_dict(df, column_name, has_booking_name='has_booking'):
-    # key: column value  value: number of bookings
-    dict_nb_bookings = dict()
-    col_list = df[column_name].unique()
-    # print(column_name)
-    # print(col_list)
-    for value in col_list:
-        values = df[train_user_df[column_name] == value][has_booking_name].values
-        dict_nb_bookings[value] = sum(values)
-
-    return dict_nb_bookings, col_list
-
-
-feature_columns = ['referer_code', 'is_app', 'agent_id', 'traffic_type', 'action_id', 'reference', 'step']
-
-for feature_column in feature_columns:
-    dict_feature_column, feature_column_list = get_nb_bookings_dict(train_user_df, feature_column)
-    print('\n --------------------')
-    print(feature_column)
-    print(feature_column_list)
-    for code, nb in dict_feature_column.items():
-        print('{}: {} #bookings: {}'.format(feature_column, code, nb))
-    print('\n --------------------\n')
-
 
 
 # ----------
@@ -252,6 +145,7 @@ target_column = ['has_booking']
 
 train_data_df = train_user_df[feature_columns + target_column]
 
+print('-------------------')
 print('train data')
 print(train_data_df.shape)
 
@@ -260,16 +154,28 @@ train_data_df = train_data_df.reindex(np.random.permutation(train_data_df.index)
 train_x = train_data_df[feature_columns]
 train_y = train_data_df[target_column].values
 
+# https://stackoverflow.com/questions/31995175/scikit-learn-cross-val-score-too-many-indices-for-array
+"""
+When we do cross validation in scikit-learn, the process requires an (R,) shape label instead of (R,1). 
+Although they are the same thing to some extend, their indexing mechanisms are different. So in your case, just add:
+c, r = labels.shape
+labels = labels.reshape(c,)
+"""
+c, r = train_y.shape
+train_y = train_y.reshape(c,)
+
 train_sub_x, val_x, train_sub_y, val_y = train_test_split(train_x, train_y, test_size=0.2, random_state=42)
 
-
+print('-------------------')
 print('train')
 print(train_x.shape)
 print(train_y.shape)
 
+
 test_x = target_user_df[feature_columns]
 # test_y = target_user_df[target_column]
 
+print('-------------------')
 print('test')
 print(test_x.shape)
 # print(test_y.shape)
@@ -298,10 +204,11 @@ def train_xgb(X_train, Y_train, hyperparameter_tuning=False, n_jobs=4, folds=3, 
     #                     objective='binary:logistic',
     #                     silent=True, nthread=nthread)
 
-    xgb_clf = XGBClassifier(nthread=n_jobs)
+    xgb_clf = XGBClassifier(nthread=n_jobs, objective='binary:logistic', silent=True,)
 
     if hyperparameter_tuning:
         params = {
+            'n_estimators': [100, 200, 400, 500],
             'min_child_weight': [1, 5, 10],
             # 'gamma': [0.5, 1, 1.5, 2, 5],
             'gamma': [0.5, 1, 1.5],
@@ -310,7 +217,7 @@ def train_xgb(X_train, Y_train, hyperparameter_tuning=False, n_jobs=4, folds=3, 
             # 'colsample_bytree': [0.6, 0.8, 1.0],
             'colsample_bytree': [0.6, 0.8],
             # 'max_depth': [3, 4, 5]
-            'max_depth': [4, 6],
+            'max_depth': [2, 4, 6],
         }
 
         skf = StratifiedKFold(n_splits=folds, shuffle=True, random_state=42)
@@ -340,6 +247,8 @@ def train_xgb(X_train, Y_train, hyperparameter_tuning=False, n_jobs=4, folds=3, 
         xgb_clf.fit(train_sub_x, train_sub_y)
 
     xgb_model_path = 'xgb.model'
+    if hyperparameter_tuning:
+        xgb_model_path = 'xgb.ht.model'
     # xgb_clf.save_model(xgb_model_path)
     joblib.dump(xgb_clf, xgb_model_path)
     print('save xgb model to {}'.format(xgb_model_path))
@@ -352,6 +261,9 @@ def train_rf(X_train, Y_train, hyperparameter_tuning=False, n_jobs=4):
     model.fit(X_train, Y_train)
 
     model_path = 'rf.model'
+    if hyperparameter_tuning:
+        model_path = 'rf.ht.model'
+
     joblib.dump(model, model_path)
     print('save rf model to {}'.format(model_path))
 
@@ -364,53 +276,39 @@ def train_nb(X_train, Y_train, hyperparameter_tuning=False):
     model.fit(X_train, Y_train,)
 
     model_path = 'gnb.model'
-    joblib.dump(model, model_path)
-    print('save GaussianNB model to {}'.format(model_path))
-
-    return model, model_path
-
-def train_lr(X_train, Y_train, hyperparameter_tuning=False, n_jobs=4):
-    model = LogisticRegression(n_jobs=n_jobs)
-    model.fit(X_train, Y_train)
-
     if hyperparameter_tuning:
-        reg = linear_model.RidgeCV(alphas=[0.1, 1.0, 10.0])
-        reg.fit(X_train, Y_train)
-        model = reg
+        model_path = 'gnb.ht.model'
 
-    model_path = 'lr.model'
     joblib.dump(model, model_path)
-
     print('save GaussianNB model to {}'.format(model_path))
 
     return model, model_path
-
-
 
 def predict(model_path, X_test):
     """
-    load the model and do prediction
+    load the model and predict unseen data
     """
     model = joblib.load(model_path)
     # y_pred = model.predict_prob(X_test)
     y_pred = model.predict(X_test)
     return y_pred
 
-def predict_blend(X_test, model_paths=['xgb.model', 'rf.model', 'nb.model'],):
+def predict_blend(X_test, model_paths=['xgb.model', 'rf.model', 'nb.model'], threshold=0.7):
     y_pred = predict(model_paths[0], X_test)
 
     for i in range(1, len(model_paths)):
         y_pred += predict(model_paths[0], X_test)
     y_pred = y_pred*1.0 / len(model_paths)
 
-    y_pred = y_pred >= 0.7
+    y_pred = y_pred >= threshold
 
     return y_pred
 
 
 
-# model, model_path = train_xgb(train_sub_x, train_sub_y, hyperparameter_tuning=False)
-# y_pred = predict('xgb.model', val_x)
+model, model_path = train_xgb(train_sub_x, train_sub_y, hyperparameter_tuning=True)
+y_pred = predict('xgb.ht.model', val_x)
+
 # model, model_path = train_rf(train_sub_x, train_sub_y, hyperparameter_tuning=False)
 # model, model_path = train_nb(train_sub_x, train_sub_y, hyperparameter_tuning=False)
 # model, model_path = train_lr(train_sub_x, train_sub_y, hyperparameter_tuning=False)
@@ -428,7 +326,6 @@ def predict_blend(X_test, model_paths=['xgb.model', 'rf.model', 'nb.model'],):
 # https://blog.myyellowroad.com/using-categorical-data-in-machine-learning-with-python-from-dummy-variables-to-deep-category-42fd0a43b009
 
 
-
 # ---------
 # Evaluate the model
 # We expect binary predictions for the target sessions, which will be evaluated by Matthews Correlation Coefficient (MCC)
@@ -440,10 +337,10 @@ def predict_blend(X_test, model_paths=['xgb.model', 'rf.model', 'nb.model'],):
 # A coefficient of +1 represents a perfect prediction, 0 an average random prediction and -1 an inverse prediction.
 # The statistic is also known as the phi coefficient. [source: Wikipedia]
 
-# y_true = val_y
+y_true = val_y
 # y_true = train_sub_y
 
-# score = matthews_corrcoef(y_true, y_pred)
-# print(score)
+score = matthews_corrcoef(y_true, y_pred)
+print(score)
 
 
